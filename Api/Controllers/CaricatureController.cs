d7f8af4f60d6c433426bfe19bec495409cce2b37
Dev-Wasim <ahwasm9@gmail.com>
2024-04-22 12:46:50 +0530

    add-upload-endPoint

---
+++ Api/Controllers/CaricatureController.cs
@@ -0,0 +1,42 @@
+﻿// Controllers/CaricatureController.cs
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CaricatureAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CaricatureController : ControllerBase
+    {
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadPhoto(IFormFile photo)
+        {
+
+            var uploadsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+            if (!Directory.Exists(uploadsFolderPath))
+            {
+                Directory.CreateDirectory(uploadsFolderPath);
+            }
+
+            var folderName = Guid.NewGuid().ToString();
+            var folderPath = Path.Combine(uploadsFolderPath, folderName);
+            Directory.CreateDirectory(folderPath);
+
+            var filePath = Path.Combine(folderPath, photo.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await photo.CopyToAsync(stream);
+            }
+
+            return Ok(new { FolderName = folderName, FilePath = filePath });
+
+        }
+
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -17,78 +17,78 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
-            options.EnableSensitiveDataLogging().UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext"),
-           sqlServerOptionsAction: sqlOptions =>
-           {
-               sqlOptions.EnableRetryOnFailure();
-           }));
+//builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+//            options.EnableSensitiveDataLogging().UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext"),
+//           sqlServerOptionsAction: sqlOptions =>
+//           {
+//               sqlOptions.EnableRetryOnFailure();
+//           }));
 
 
-builder.Services.AddScoped<IDropdownManager, DropdownManager>();
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<IDropdownManager, DropdownManager>();
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>()
+//    .AddDefaultTokenProviders();
+//builder.Services.AddScoped<IUserService, UserService>();
 
-builder.Services.AddScoped<IUserManager, UserManager>();
-builder.Services.AddScoped<IAccountManager, AccountManager>();
-builder.Services.AddScoped<IDashboardManager, DashboardManager>();
-builder.Services.AddScoped<IIdentityTokenService, IdentityTokenService>();
-builder.Services.AddScoped<ILogExceptionService, LogExceptionService>();
-builder.Services.AddScoped<ILogActivityService, LogActivityService>();
-builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+//builder.Services.AddScoped<IUserManager, UserManager>();
+//builder.Services.AddScoped<IAccountManager, AccountManager>();
+//builder.Services.AddScoped<IDashboardManager, DashboardManager>();
+//builder.Services.AddScoped<IIdentityTokenService, IdentityTokenService>();
+//builder.Services.AddScoped<ILogExceptionService, LogExceptionService>();
+//builder.Services.AddScoped<ILogActivityService, LogActivityService>();
+//builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddAutoMapper(typeof(Program));
-var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+//var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(x =>
-    {
-        x.RequireHttpsMetadata = false;
-        x.SaveToken = true;
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-            "Example: \"Bearer hvvhvh664b\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Scheme = "Bearer"
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header
-            },
-            new List<string>()
-        }
-    });
+//builder.Services.AddAuthentication(x =>
+//{
+//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//    .AddJwtBearer(x =>
+//    {
+//        x.RequireHttpsMetadata = false;
+//        x.SaveToken = true;
+//        x.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+//            ValidateIssuer = false,
+//            ValidateAudience = false
+//        };
+//    });
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//    {
+//        Description =
+//            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+//            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+//            "Example: \"Bearer hvvhvh664b\"",
+//        Name = "Authorization",
+//        In = ParameterLocation.Header,
+//        Scheme = "Bearer"
+//    });
+//    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                            {
+//                                Type = ReferenceType.SecurityScheme,
+//                                Id = "Bearer"
+//                            },
+//                Scheme = "oauth2",
+//                Name = "Bearer",
+//                In = ParameterLocation.Header
+//            },
+//            new List<string>()
+//        }
+//    });
 
-});
+//});
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
@@ -111,7 +111,7 @@ app.UseCors();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseMiddleware<LoggingMiddleware>();
+//app.UseMiddleware<LoggingMiddleware>();
 /*app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
@@ -121,14 +121,14 @@ app.UseMiddleware<LoggingMiddleware>();
 
 
 
-Seed();
+//Seed();
 app.MapControllers();
-void Seed()
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var db_initailizer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-        db_initailizer.Initialize();
-    }
-}
+//void Seed()
+//{
+//    using (var scope = app.Services.CreateScope())
+//    {
+//        var db_initailizer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+//        db_initailizer.Initialize();
+//    }
+//}
 app.Run();
